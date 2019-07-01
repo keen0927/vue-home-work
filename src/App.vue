c10f3eb7d11d47595bc3957dc0cb8e08fe4737b3
KEEN <kyh0927@live.co.kr>
2019-07-02 00:17:16 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <router-link :to="{name: 'home'}">home</router-link>
-    <router-link :to="{name: 'router'}">router</router-link>
-    <router-view></router-view>
+    <ul class="nav">
+      <li><router-link :to="{name: 'home'}" exact>home</router-link></li>
+      <li><router-link :to="{name: 'router'}">router</router-link></li>
+    </ul>
+    <router-view class="router-wrap"></router-view>
   </div>
 </template>
 
@@ -12,13 +14,30 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('./scss/main.scss');
+
+.nav {
+  border-bottom: 1px solid rgba(0,0,0,0.52);
+  margin: 0 15px 15px;
+  padding: 15px 0;
+  li {
+    display: inline-block;
+    margin-right: 10px;
+    a {
+      display: inline-block;
+      padding: 10px 15px;
+      text-decoration: none;
+      border:1px solid rgba(0,0,0,0.28);
+      color: rgba(0,0,0,0.52);
+      &.is-active {
+        background: rgba(0,0,0,0.52);
+        color: #fff;
+      }
+    }
+  }
+}
+.router-wrap {
+  padding: 0 15px;
 }
 </style>
--- src/pages/Router.vue
+++ src/pages/Router.vue
@@ -1,12 +1,49 @@
 <template>
 	<div>
-롸우터
+		<h1>Vue-router</h1>
+		<p>- 중첩된 경로, 뷰를 매핑할 수 있다.</p>
+		<p>- 컴포넌트 기반의 라우팅을 구혈할 수 있다.</p>
+		<p>- Transitino을 적용할 수 있다</p>
+		<p>- 히스토리모드와 해시모드를 사용할 수 있다</p>
+		<p>- 쿼리스트링, 파라미터, 와일드 카드를 사용하여 구혈할 수 있다.</p>
+		<p v-text="textRouterLink"></p>
+		<p v-text="importRouter"></p>
+		<p v-text="constRouter"></p>
+		<p>router-view에는 매핑한 컴포넌트가 나타나게 된다.</p>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'Router'
+		name: 'Router',
+		created () {
+			const ddd = this.$route.path;
+			console.log(ddd);
+		},
+		data() {
+			return {
+				textRouterLink: '<router-link to="/home"></router-link>',
+				importRouter: 'import VueRouter from "vue-router"',
+				constRouter: `
+					const router = new VueRouter({
+						routes: [
+							{ path: '/', component: Home },
+							{ path: '/home', component: Home },
+							{ path: '/about', component: About },
+						]
+					});
+					export default {
+						name: 'app',
+						router
+					}
+				`,
+				contactlist: [
+					{ no:1001, name:'킨1', tel:'010-3333-3333' },
+					{ no:1002, name:'킨2', tel:'010-3333-3333' },
+					{ no:1003, name:'킨3', tel:'010-3333-3333' },
+				]
+			}
+		},
 	}
 </script>
 
