900a1199f7abb0f06fa8aeedd1aff52dc882cd86
KEEN <kyh0927@live.co.kr>
2019-07-04 01:10:39 +0900

    1

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,61 @@
 <template>
 	<div>
-		Home
 		
+		
+		<ul>
+			<li v-for="(list,index) in lists" :key="index">
+				{{ list.title }}
+				<button type="button" @click="deleteList(index)">DELETE</button>
+				<button type="button" @click="editList(index)">Edit</button>
+				<div v-if="list.editInput">
+					<input type="text" v-model="editValue" placeholder="수정할 내용을 입력하세요.">
+					<button type="button" @click="editReg(index)">수정</button>
+				</div>
+			</li>
+		</ul>
 
+		<p>
+			<input type="text" v-model="inputValue">
+			<button type="button" ref="reg" @click="regList">등록</button>
+		</p>
 
-
+		{{inputValue}}
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'Home'
+		name: 'Home',
+		data() {
+			return {
+				lists: [
+					{title: '제목1',editInput:false},
+					{title: '제목2',editInput:false},
+					{title: '제목3',editInput:false},
+					{title: '제목4',editInput:false},
+				],
+				inputValue: "",
+				editValue: ""
+			}
+		},
+		methods: {
+			regList() {
+				this.lists.push({title: this.inputValue, editInput:false});
+				this.inputValue = "";
+			},
+			deleteList(index) {
+				this.lists.splice(index,1)
+			},
+			editList(index) {
+				console.log('editList')
+				this.lists[index].editInput = true;
+			},
+			editReg(index) {
+				this.lists[index].title = this.editValue;
+				this.editValue = "";
+				this.lists[index].editInput = false;
+			}
+		},
 	}
 </script>
 
