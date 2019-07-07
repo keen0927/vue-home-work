5a84788cf0448a63f2894dd8188433dcb67fbf5a
KEEN <kyh0927@live.co.kr>
2019-07-07 23:10:16 +0900

    1

--- src/components/RegionButtons.vue
+++ src/components/RegionButtons.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-Region
+        <!-- <button class="region" v-for='region in regions'
+            :class="isSelected(region)"
+            @click="changeRegion({region:region});">
+            {{region}}
+        </button> -->
     </div>
 </template>
 
 <script>
+import Constants from '../Constants';
+import _ from 'lodash';
+import { mapGetters, mapMutations } from 'vuex';
+
     export default {
-        name: 'RegionButtons'
+        name: 'RegionButtons',
+        computed: mapGetters([
+            'regions',
+            'currentRegion'       
+        ]),
+        methods: 
+            _.extend({
+                isSelected(region) {
+                    if (region == this.currentRegino) return { selected: true};
+                    else return { selected: false}
+                }
+            },
+            mapMutations([
+                Constants.CHANGE_REGION
+            ])
+        )
     }
 </script>
 
