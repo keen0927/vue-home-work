88007e7e3b01369627fc66e25522023f1c8e17ef
KEEN <kyh0927@live.co.kr>
2019-07-09 23:45:22 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
-    <div>dddd
-        <region-buttons></region-buttons>
-        <country-list></country-list>
+    <div>
+        {{ count }}
+        <button @click="increment" >증가</button>
+        <button @click="decrement" >감소</button>
+
+        <p>모든 유저 : {{ $store.getters.allUsersLength }}</p>
     </div>
 </template>
 
 <script>
-import RegionButtons from './components/RegionButtons.vue';
-import CountryList from './components/CountryList.vue';
+// import RegionButtons from './components/RegionButtons.vue';
+// import CountryList from './components/CountryList.vue';
+
     export default {
         name: 'App',
         components: {
-            RegionButtons,
-            CountryList
-        }
+            // RegionButtons,
+            // CountryList
+        },
+        computed: {
+            count() {
+                return this.$store.state.count
+            }
+        },
+        methods: {
+            increment() {
+                this.$store.commit('increment');
+            },
+            decrement() {
+                this.$store.commit('decrement');
+            }
+        },
     }
 </script>
 
