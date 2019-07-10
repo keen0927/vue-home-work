e7ee6844480eebcec128f5852d2bcb17a9296716
KEEN <kyh0927@live.co.kr>
2019-07-11 00:28:33 +0900

    1

---
+++ src/components/Red.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="red">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Red'
+    }
+</script>
+
+<style lang="scss" scoped>
+.red {
+    padding: 50px;
+    border: 3px solid red;
+}
+</style>
